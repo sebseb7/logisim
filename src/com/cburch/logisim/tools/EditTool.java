5604975befff8472c507d1c42922e47307f3d1a1
Kevin Walsh <kwalsh@holycross.edu>
2016-11-19 16:46:25 -0500

    attempt to paint less when moving mouse

--- src/com/cburch/logisim/tools/EditTool.java
+++ src/com/cburch/logisim/tools/EditTool.java
@@ -446,15 +446,17 @@ public class EditTool extends Tool {
 				if (o != null) {
 					lastX = snapx;
 					lastY = snapy;
-					canvas.repaint();
+					Location oldWireLoc = wireLoc;
 					boolean ret = ((Boolean) o).booleanValue();
 					wireLoc = ret ? snap : NULL_LOCATION;
+					repaintIndicators(canvas, oldWireLoc, wireLoc);
 					return ret;
 				}
 			} else {
 				cache.clear();
 			}
 
+			Location oldWireLoc = wireLoc; 
 			boolean ret = isEligible && isWiringPoint(canvas, snap, mods);
 			wireLoc = ret ? snap : NULL_LOCATION;
 			cache.put(snap, Boolean.valueOf(ret));
@@ -468,9 +470,18 @@ public class EditTool extends Tool {
 
 			lastX = snapx;
 			lastY = snapy;
-			canvas.repaint();
+			repaintIndicators(canvas, oldWireLoc, wireLoc);
 			return ret;
 		}
+	}
+
+	private void repaintIndicators(Canvas canvas, Location a, Location b) {
+		if (a.equals(b))
+			return;
+		if (a != NULL_LOCATION)
+			canvas.repaint(a.getX()-6, a.getY()-6, 12, 12);
+		if (b != NULL_LOCATION)
+			canvas.repaint(b.getX()-6, b.getY()-6, 12, 12);
 	}
 
 	private boolean updateLocation(Canvas canvas, KeyEvent e) {
