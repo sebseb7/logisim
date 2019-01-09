b73cf941ccf042dbe21a793ec001069adb044774
Kevin Walsh <kwalsh@holycross.edu>
2019-01-09 16:25:24 -0500

    antialias appearance editor layout popup

--- src/com/cburch/logisim/gui/appear/LayoutThumbnail.java
+++ src/com/cburch/logisim/gui/appear/LayoutThumbnail.java
@@ -32,8 +32,9 @@ package com.cburch.logisim.gui.appear;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Graphics;
+import java.awt.RenderingHints;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -71,6 +72,14 @@ public class LayoutThumbnail extends JComponent {
   @Override
   protected void paintComponent(Graphics g) {
     if (circuitState != null) {
+      Graphics2D g2 = (Graphics2D)g;
+      g2.setRenderingHint(
+          RenderingHints.KEY_TEXT_ANTIALIASING,
+          RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+      g2.setRenderingHint(
+          RenderingHints.KEY_ANTIALIASING,
+          RenderingHints.VALUE_ANTIALIAS_ON);
+
       Circuit circuit = circuitState.getCircuit();
       Bounds bds = circuit.getBounds(g);
       Dimension size = getSize();
