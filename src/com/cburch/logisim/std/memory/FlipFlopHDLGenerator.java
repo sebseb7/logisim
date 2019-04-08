29cbb03164676f1eb28abaaa44a60bff43d35fdb
Kevin Walsh <kwalsh@holycross.edu>
2019-04-08 14:47:23 -0400

    lcd_demo works

--- src/com/cburch/logisim/std/memory/FlipFlopHDLGenerator.java
+++ src/com/cburch/logisim/std/memory/FlipFlopHDLGenerator.java
@@ -48,7 +48,7 @@ public class FlipFlopHDLGenerator extends HDLGenerator {
     for (String port : inportNames)
       inPorts.add(port, 1, portnum++, false);
 
-    clockPort = new ClockPortInfo("GlobalClock", "ClockEnable", portnum++);
+    clockPort = new ClockPortInfo("Clock", "Enable", portnum++);
     outPorts.add("Q", 1, portnum++, null);
     outPorts.add("Q_bar", 1, portnum++, null);
     inPorts.add("Reset", 1, portnum++, false);
@@ -66,15 +66,15 @@ public class FlipFlopHDLGenerator extends HDLGenerator {
       out.stmt();
       out.stmt(vhdlUpdate);
       out.stmt();
-      out.stmt("make_memory : PROCESS( clock , Reset , Preset , ClockEnable , s_next_state )");
+      out.stmt("make_memory : PROCESS( Clock , Reset , Preset , Enable , s_next_state )");
       out.stmt("BEGIN");
       out.stmt("   IF (Reset = '1') THEN s_current_state_reg <= '0';");
       out.stmt("   ELSIF (Preset = '1') THEN s_current_state_reg <= '1';");
       if (edgeTriggered())
-        out.stmt("   ELSIF (GlobalClock'event AND (GlobalClock = '1')) THEN");
+        out.stmt("   ELSIF (Clock'event AND (Clock = '1')) THEN");
       else
-        out.stmt("   ELSIF (GlobalClock = '1') THEN");
-      out.stmt("      IF (ClockEnable = '1') THEN");
+        out.stmt("   ELSIF (Clock = '1') THEN");
+      out.stmt("      IF (Enable = '1') THEN");
       out.stmt("         s_current_state_reg <= s_next_state;");
       out.stmt("      END IF;");
       out.stmt("   END IF;");
@@ -93,18 +93,18 @@ public class FlipFlopHDLGenerator extends HDLGenerator {
       out.stmt("end");
       out.stmt();
       if (edgeTriggered()) {
-        out.stmt("always @(posedge Reset or posedge Preset or posedge GlobalClock)");
+        out.stmt("always @(posedge Reset or posedge Preset or posedge Clock)");
         out.stmt("begin");
         out.stmt("   if (Reset) s_current_state_reg <= 1'b0;");
         out.stmt("   else if (Preset) s_current_state_reg <= 1'b1;");
-        out.stmt("   else if (ClockEnable) s_current_state_reg<= s_next_state;");
+        out.stmt("   else if (Enable) s_current_state_reg<= s_next_state;");
         out.stmt("end");
       } else {
         out.stmt("always @(*)");
         out.stmt("begin");
         out.stmt("   if (Reset) s_current_state_reg <= 1'b0;");
         out.stmt("   else if (Preset) s_current_state_reg <= 1'b1;");
-        out.stmt("   else if (ClockEnable & (GlobalClock == 1)) s_current_state_reg <= s_next_state;");
+        out.stmt("   else if (Enable & (Clock == 1)) s_current_state_reg <= s_next_state;");
         out.stmt("end");
       }
     }
